00e0c9b0c5dc9a9f0efee60b71efd2d83763900b
Edmond <edmond.santiago7@gmail.com>
2024-01-26 17:17:48 +1100

    add delete feature

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
+/*function fetchData(url, method, body) {
+  fetch(url, {
+    method: method,
+    headers: {
+      "Content-Type": "application/json",
+      "Accept": "application/json",
+    },
+    body: JSON.stringify(body)
+  })
+}*/
+const quoteList = document.querySelector("#quote-list");
+
 fetch("http://localhost:3000/quotes?_embed=likes")
   .then(res => res.json())
   .then(quotes => renderQuotes(quotes))
@@ -9,10 +21,8 @@ function renderQuotes(quotes) {
 
 //Show quotes on screen
 function renderQuote(quote) {
-  const quoteList = document.querySelector("#quote-list");
   const quoteLi = document.createElement("li");
   quoteLi.className = "quote-card";
-  console.log(quote)
   quoteLi.innerHTML = `
     <blockquote class="blockquote">
     <p class="mb-0">${quote.quote}</p>
@@ -23,4 +33,45 @@ function renderQuote(quote) {
     </blockquote>
   `
   quoteList.append(quoteLi);
+
+  quoteLi.querySelector(".btn-danger").addEventListener("click", () => handleDeleteClick(quote))
+}
+
+//Submit new quote
+const quoteForm = document.querySelector("#new-quote-form");
+quoteForm.addEventListener("submit", (e) => handleSubmit(e));
+
+function handleSubmit(e) {
+  e.preventDefault();
+  const newQuote = e.target["new-quote"].value
+  const newAuthor = e.target.author.value
+
+  fetch("http://localhost:3000/quotes", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "Accept": "application/json",
+    },
+    body: JSON.stringify({
+      quote: newQuote,
+      author: newAuthor
+    })
+  })
+    .then(res => res.json())
+    .then(quote => renderQuote(quote))
+  e.target.reset();
+}
+
+//Delete quote
+function handleDeleteClick(quote) {
+  fetch(`http://localhost:3000/quotes/${quote.id}`, {
+    method: "DELETE"
+  })
+    .then(res => res.json())
+    .then(() => {
+      quoteList.innerHTML = "";
+      fetch("http://localhost:3000/quotes?_embed=likes")
+        .then(res => res.json())
+        .then(quotes => renderQuotes(quotes))
+    })
 }
