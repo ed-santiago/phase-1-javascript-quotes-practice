34093c4e32f51effdba4a6067a854ccaf6c4764e
Edmond <edmond.santiago7@gmail.com>
2024-01-27 18:13:58 +1100

    add edit quote feature

--- src/index.js
+++ src/index.js
@@ -26,17 +26,66 @@ function renderQuote(quote) {
   quoteLi.className = "quote-card";
   quoteLi.innerHTML = `
     <blockquote class="blockquote">
-    <p class="mb-0">${quote.quote}</p>
-    <footer class="blockquote-footer">${quote.author}</footer>
-    <br>
-    <button class='btn-success'>Likes: <span>0</span></button>
-    <button class='btn-danger'>Delete</button>
+      <p class="mb-0">${quote.quote}</p>
+      <footer class="blockquote-footer">${quote.author}</footer>
+      <br>
+      <button class='btn-success'>Likes: <span>0</span></button>
+      <button class='btn-danger'>Delete</button>
+      <button class='btn-edit'>Edit</button>
     </blockquote>
+    <form id="edit-form" style="display: none;">
+      <div>
+        <label for="edit-quote">Edit Quote</label>
+        <textarea id="edit-quote" name="edit-quote" rows="3" cols="80" placeholder="Edit quote"></textarea>
+      </div>
+      <label for="edit-author">Edit Author</label>
+      <input id="edit-author" name="edit-author" type="text" placeholer="edit quote" />
+      <input type="submit" value="Submit" />
+    </form>
   `
   quoteList.append(quoteLi);
 
   quoteLi.querySelector(".btn-success").addEventListener("click", () => handleLikeClick(quote));
   quoteLi.querySelector(".btn-danger").addEventListener("click", () => handleDeleteClick(quote));
+  quoteLi.querySelector(".btn-edit").addEventListener("click", (e) => {
+    const editForm = quoteLi.querySelector("#edit-form");
+    const editQuote = quoteLi.querySelector("#edit-quote");
+    const editAuthor = quoteLi.querySelector("#edit-author");
+    if (editForm.style.display === "none") {
+      editForm.style.display = "block";
+      editQuote.value = quote.quote;
+      editAuthor.value = quote.author;
+      const id = quote.id;
+      editForm.addEventListener("submit", e => handleEditSubmit(e, id))
+    }
+    else
+      editForm.style.display = "none";
+  });
+}
+
+//Submit edited quote
+function handleEditSubmit(e, id) {
+  const editedQuote = e.target["edit-quote"].value;
+  const editedAuthor = e.target["edit-author"].value;
+
+  fetch(`http://localhost:3000/quotes/${id}`, {
+    method: "PATCH",
+    headers: {
+      "Content-Type": "application/json",
+      "Accept": "application/json",
+    },
+    body: JSON.stringify({
+      quote: editedQuote,
+      author: editedAuthor
+    })
+  })
+    .then(res => res.json())
+    .then(() => {
+      quoteList.innerHTML = "";
+      fetch("http://localhost:3000/quotes?_embed=likes")
+        .then(res => res.json())
+        .then(quotes => renderQuotes(quotes))
+    })
 }
 
 //Submit new quote
